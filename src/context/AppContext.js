907decb1f3e09f3e7aa0dbf859da0229b7199116
rsimoneau01 <145231060+rsimoneau01@users.noreply.github.com>
2023-10-02 14:46:12 -0700

    Update AppContext.js

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -31,6 +31,31 @@ export const AppReducer = (state, action) => {
                 }
             }
             case 'RED_EXPENSE':
+            total_budget = state.expenses.reduce(
+                (previousExp, currentExp) => {
+                    return previousExp - currentExp.cost
+                },0
+            );
+            total_budget = total_budget + action.payload.cost;
+            action.type = "DONE";
+            if(total_budget <= state.budget) {
+                total_budget = 0;
+                state.expenses.map((currentExp)=> {
+                    if(currentExp.name === action.payload.name) {
+                        currentExp.cost = action.payload.cost - currentExp.cost;
+                    }
+                    return currentExp
+                });
+                return {
+                    ...state,
+                };
+            } else {
+                alert("Cannot decrease the allocation! Out of funds");
+                return {
+                    ...state
+                }
+            }
+            case 'RED_EXPENSE':
                 const red_expenses = state.expenses.map((currentExp)=> {
                     if (currentExp.name === action.payload.name && currentExp.cost - action.payload.cost >= 0) {
                         currentExp.cost =  currentExp.cost - action.payload.cost;
@@ -78,7 +103,7 @@ export const AppReducer = (state, action) => {
 
 // 1. Sets the initial state when the app loads
 const initialState = {
-    budget: 2000,
+    budget: onclick,
     expenses: [
         { id: "Marketing", name: 'Marketing', cost: 50 },
         { id: "Finance", name: 'Finance', cost: 300 },
